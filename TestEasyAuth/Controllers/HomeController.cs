de39a6354bc4c04b391236fb80372447377f4a5a
Kirk Evans <kirke@microsoft.com>
2018-02-27 15:33:39 -0600

    Add project files.

---
+++ TestEasyAuth/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestEasyAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            System.Diagnostics.Trace.TraceInformation("HomeController::Index entered in HomeController.cs");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            System.Diagnostics.Trace.TraceInformation("HomeController::About entered in HomeController.cs");
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            System.Diagnostics.Trace.TraceInformation("HomeController::Contact entered in HomeController.cs");
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
